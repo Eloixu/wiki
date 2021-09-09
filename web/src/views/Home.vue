8ae9409a51e15225885a61c342a91fce21cc6b1b
EloiXu <robot0018@126.com>
2021-09-09 17:57:19 +0800

    5-4 电子书布局调整，一行显示三列电子书

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,77 +1,62 @@
 <template>
-  <a-layout>
-    <a-layout-sider width="200" style="background: #fff">
-    <a-menu
-            mode="inline"
-            v-model:selectedKeys="selectedKeys2"
-            v-model:openKeys="openKeys"
-            :style="{ height: '100%', borderRight: 0 }"
-    >
-      <a-sub-menu key="sub1">
-        <template #title>
-          <span><user-outlined />subnav 1</span>
-        </template>
-        <a-menu-item key="1">option1</a-menu-item>
-        <a-menu-item key="2">option2</a-menu-item>
-        <a-menu-item key="3">option3</a-menu-item>
-        <a-menu-item key="4">option4</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub2">
-        <template #title>
-          <span><laptop-outlined />subnav 2</span>
-        </template>
-        <a-menu-item key="5">option5</a-menu-item>
-        <a-menu-item key="6">option6</a-menu-item>
-        <a-menu-item key="7">option7</a-menu-item>
-        <a-menu-item key="8">option8</a-menu-item>
-      </a-sub-menu>
-      <a-sub-menu key="sub3">
-        <template #title>
-          <span><notification-outlined />subnav 3</span>
-        </template>
-        <a-menu-item key="9">option9</a-menu-item>
-        <a-menu-item key="10">option10</a-menu-item>
-        <a-menu-item key="11">option11</a-menu-item>
-        <a-menu-item key="12">option12</a-menu-item>
-      </a-sub-menu>
-    </a-menu>
-  </a-layout-sider>
-    <a-layout-content :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
-        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-            <template #footer>
-                <div>
-                    <b>ant design vue</b>
-                    footer part
-                </div>
-            </template>
-            <template #renderItem="{ item }">
-                <a-list-item key="item.title">
-                    <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
+    <a-layout>
+        <a-layout-sider width="200" style="background: #fff">
+            <a-menu
+                    mode="inline"
+                    :style="{ height: '100%', borderRight: 0 }"
+            >
+                <a-sub-menu key="sub1">
+                    <template #title>
+                        <span><user-outlined />subnav 11111</span>
                     </template>
-                    <template #extra>
-                        <img
-                                width="272"
-                                alt="logo"
-                                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                        />
+                    <a-menu-item key="1">option1</a-menu-item>
+                    <a-menu-item key="2">option2</a-menu-item>
+                    <a-menu-item key="3">option3</a-menu-item>
+                    <a-menu-item key="4">option4</a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu key="sub2">
+                    <template #title>
+                        <span><laptop-outlined />subnav 2</span>
                     </template>
-                    <a-list-item-meta :description="item.description">
-                        <template #title>
-                            <a :href="item.href">{{ item.title }}</a>
+                    <a-menu-item key="5">option5</a-menu-item>
+                    <a-menu-item key="6">option6</a-menu-item>
+                    <a-menu-item key="7">option7</a-menu-item>
+                    <a-menu-item key="8">option8</a-menu-item>
+                </a-sub-menu>
+                <a-sub-menu key="sub3">
+                    <template #title>
+                        <span><notification-outlined />subnav 3</span>
+                    </template>
+                    <a-menu-item key="9">option9</a-menu-item>
+                    <a-menu-item key="10">option10</a-menu-item>
+                    <a-menu-item key="11">option11</a-menu-item>
+                    <a-menu-item key="12">option12</a-menu-item>
+                </a-sub-menu>
+            </a-menu>
+        </a-layout-sider>
+        <a-layout-content
+                :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+        >
+            <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+                <template #renderItem="{ item }">
+                    <a-list-item key="item.name">
+                        <template #actions>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
                         </template>
-                        <template #avatar><a-avatar :src="item.avatar" /></template>
-                    </a-list-item-meta>
-                    {{ item.content }}
-                </a-list-item>
-            </template>
-        </a-list>
-
-    </a-layout-content>
-  </a-layout>
+                        <a-list-item-meta :description="item.description">
+                            <template #title>
+                                <a :href="item.href">{{ item.name }}</a>
+                            </template>
+                            <template #avatar><a-avatar :src="item.cover"/></template>
+                        </a-list-item-meta>
+                    </a-list-item>
+                </template>
+            </a-list>
+        </a-layout-content>
+    </a-layout>
 </template>
 
 <script lang="ts">
@@ -102,7 +87,7 @@ export default defineComponent({
 
     onMounted(()=>{
       console.log("onMounted");
-      axios.get("http://127.0.0.1:8880/ebook/list?name=Spring").then((response) => {
+      axios.get("http://127.0.0.1:8880/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
