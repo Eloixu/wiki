12e2b4bbbd52b246e982f55305512a8e2a4e54ba
EloiXu <robot0018@126.com>
2021-09-14 23:23:51 +0800

    7-7 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -4,33 +4,21 @@
             <a-menu
                     mode="inline"
                     :style="{ height: '100%', borderRight: 0 }"
+                    @click="handleClick"
             >
-                <a-sub-menu key="sub1">
-                    <template #title>
-                        <span><user-outlined />subnav 11111</span>
+                <a-menu-item key="welcome">
+                    <router-link :to="'/'">
+                        <MailOutlined />
+                        <span>欢迎</span>
+                    </router-link>
+                </a-menu-item>
+                <a-sub-menu v-for="item in level1" :key="item.id">
+                    <template v-slot:title>
+                        <span><user-outlined />{{item.name}}</span>
                     </template>
-                    <a-menu-item key="1">option1</a-menu-item>
-                    <a-menu-item key="2">option2</a-menu-item>
-                    <a-menu-item key="3">option3</a-menu-item>
-                    <a-menu-item key="4">option4</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub2">
-                    <template #title>
-                        <span><laptop-outlined />subnav 2</span>
-                    </template>
-                    <a-menu-item key="5">option5</a-menu-item>
-                    <a-menu-item key="6">option6</a-menu-item>
-                    <a-menu-item key="7">option7</a-menu-item>
-                    <a-menu-item key="8">option8</a-menu-item>
-                </a-sub-menu>
-                <a-sub-menu key="sub3">
-                    <template #title>
-                        <span><notification-outlined />subnav 3</span>
-                    </template>
-                    <a-menu-item key="9">option9</a-menu-item>
-                    <a-menu-item key="10">option10</a-menu-item>
-                    <a-menu-item key="11">option11</a-menu-item>
-                    <a-menu-item key="12">option12</a-menu-item>
+                    <a-menu-item v-for="child in item.children" :key="child.id">
+                        <MailOutlined /><span>{{child.name}}</span>
+                    </a-menu-item>
                 </a-sub-menu>
             </a-menu>
         </a-layout-sider>
@@ -62,6 +50,8 @@
 <script lang="ts">
 import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios from 'axios';
+import { message } from 'ant-design-vue';
+import {Tool} from "@/util/tool";
 
 const listData: Record<string, string>[] = [];
 
@@ -83,7 +73,33 @@ export default defineComponent({
     console.log("setup");
 
     const ebooks = ref();
-    const ebooks1 = reactive({books:[]});
+//    const ebooks1 = reactive({books:[]});
+
+      /**
+       * 分类数据查询
+       **/
+    const level1 = ref();
+    const handleQueryCategory = () => {
+        axios.get("/category/all").then((response) => {
+            const data = response.data;
+            if(data.success){
+                //把response里的data的值赋值给categorys
+                const categorys = data.content;
+                console.log("原始数组：", categorys);
+
+                level1.value = [];
+                level1.value = Tool.array2Tree(categorys, 0);
+                console.log("树形结构：", level1);
+            }else{
+                message.error(data.message);
+            }
+
+        });
+    };
+
+    const handleClick = () => {
+        console.log("menu click")
+    };
 
     onMounted(()=>{
       console.log("onMounted");
@@ -97,6 +113,8 @@ export default defineComponent({
         ebooks.value = data.content.list;
 //        ebooks1.books = data.content;
       });
+        handleQueryCategory();
+
     });
 
     return{
@@ -114,6 +132,8 @@ export default defineComponent({
             { type: 'LikeOutlined', text: '156' },
             { type: 'MessageOutlined', text: '2' },
         ],
+        handleClick,
+        level1
     }
   }
 });
