02c08bfe2cfc2f7943b22019fa6830287806f7c5
EloiXu <robot0018@126.com>
2021-09-15 17:41:52 +0800

    7-8 home默认在内容部分显示欢迎页面 点击欢迎菜单时可以显示欢迎页面 点击分类菜单时可以显示电子书（只有二级分类才可以触发click事件）

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -7,10 +7,8 @@
                     @click="handleClick"
             >
                 <a-menu-item key="welcome">
-                    <router-link :to="'/'">
-                        <MailOutlined />
-                        <span>欢迎</span>
-                    </router-link>
+                    <MailOutlined />
+                    <span>欢迎</span>
                 </a-menu-item>
                 <a-sub-menu v-for="item in level1" :key="item.id">
                     <template v-slot:title>
@@ -25,7 +23,10 @@
         <a-layout-content
                 :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
-            <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+            <div class="welcome" v-show="isShowWelcome">
+                <h1>欢迎使用Eloi's wiki</h1>
+            </div>
+            <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks" v-show="!isShowWelcome">
                 <template #renderItem="{ item }">
                     <a-list-item key="item.name">
                         <template #actions>
@@ -97,8 +98,17 @@ export default defineComponent({
         });
     };
 
-    const handleClick = () => {
-        console.log("menu click")
+
+      //是否显示欢迎页面
+      const isShowWelcome = ref(true);
+
+      const handleClick = (value: any) => {
+        console.log("menu click", value)
+        if(value.key === 'welcome'){
+            isShowWelcome.value = true;
+        }else{
+            isShowWelcome.value = false;
+        }
     };
 
     onMounted(()=>{
@@ -133,7 +143,8 @@ export default defineComponent({
             { type: 'MessageOutlined', text: '2' },
         ],
         handleClick,
-        level1
+        level1,
+        isShowWelcome
     }
   }
 });
