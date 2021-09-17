6d231ca93cff1880370567226b1db3d4bcf69503
EloiXu <robot0018@126.com>
2021-09-17 15:34:41 +0800

    bugfix

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -18,5 +18,6 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     'vue/no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/ban-types': 0,
   }
 }
