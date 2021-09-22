fd54f308b870bbb43c3d292ff510f7ebca6262ee
EloiXu <robot0018@126.com>
2021-09-22 23:14:05 +0800

    9-12 前后端增加登录拦截 前端：axios拦截器，把token放入每个请求的header 后端：spring拦截器，从请求的header里取出token，判断用户是否登录了

--- src/main/java/com/pwc/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/pwc/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,34 @@
-//package com.pwc.wiki.config;
-//
+package com.pwc.wiki.config;
+
 //import com.pwc.wiki.interceptor.LogInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//import javax.annotation.Resource;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
+import com.pwc.wiki.interceptor.LoginInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
 //    @Resource
 //    LogInterceptor logInterceptor;
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+
+    @Autowired
+    LoginInterceptor loginInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/find-content/**"
+                );
+    }
+}
