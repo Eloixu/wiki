252ec0489d898f49df743d1bcec44744e931cfa7
EloiXu <robot0018@126.com>
2021-10-17 23:16:53 +0800

    11-7 后端增加获取统计数值接口

--- src/main/java/com/pwc/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/pwc/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,8 +1,14 @@
 package com.pwc.wiki.mapper;
 
+import com.pwc.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 /**
  * Created by xuhaocheng on 14/10/2021.
  */
 public interface EbookSnapshotMapperCust {
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
